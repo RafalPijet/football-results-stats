6336598d08fc20b3d588c5f84352340be846fb31
Rafal Pijet <stronglopez@wp.pl>
2021-01-15 10:20:01 +0100

    File reading handling with composition solution

--- build/index.js
+++ build/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
-console.log(reader.data);
+var CsvFileReader_1 = require("./CsvFileReader");
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+console.log(matchReader.matches);
